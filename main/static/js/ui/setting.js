59e127e702898db253f4eddff75659b4e3469dc0
air158 <727626783@qq.com>
2024-08-18 16:28:24 +0800

    main css

--- main/static/js/ui/setting.js
+++ main/static/js/ui/setting.js
@@ -1,18 +1,18 @@
-// 添加提示界面关闭逻辑
-document.getElementById('tooltip-close').addEventListener('click', function() {
-    tooltip.style.display = 'none';
-    overlay.style.display = 'none';
-});
+// // 添加提示界面关闭逻辑
+// document.getElementById('tooltip-close').addEventListener('click', function() {
+//     tooltip.style.display = 'none';
+//     overlay.style.display = 'none';
+// });
 
-overlay.addEventListener('click', function() {
-    tooltip.style.display = 'none';
-    overlay.style.display = 'none';
-});
+// overlay.addEventListener('click', function() {
+//     tooltip.style.display = 'none';
+//     overlay.style.display = 'none';
+// });
 
-// 点击提示界面外部关闭提示界面
-document.addEventListener('click', function(event) {
-    if (!tooltip.contains(event.target)) {
-        tooltip.style.display = 'none';
-        overlay.style.display = 'none';
-    }
-});
+// // 点击提示界面外部关闭提示界面
+// document.addEventListener('click', function(event) {
+//     if (!tooltip.contains(event.target)) {
+//         tooltip.style.display = 'none';
+//         overlay.style.display = 'none';
+//     }
+// });
